879afcfbeaf0191c8e567f96cf948c02d899ecb8
Infdevv <bkspears@outlook.com>
2024-02-23 19:37:18 -0600

    etstes

--- sw.js
+++ sw.js
@@ -1,19 +1,18 @@
-// Jesse, we need to cook 
-
-
 self.addEventListener('fetch', event => {
   // if request comes from this origin, dont intercept it. if it goes to other origins, intercept it.
-  const url = self.location ? self.location.href : ''; // Get the URL of the service worker environment
+  url = window.location.href
   if (event.request.url.startsWith(url.split("/")[0])) {
     return;
   }
-  console.log('Intercepting request for:', event.request.url);
+  console.log('Intercepting  request for:', event.request.url);
 
   event.respondWith(
     fetch("https://axiomhub.net/main.php?url=" + event.request.url)
       .then(function(response) {
+
         console.log("response", response);
+        // return response;
         return response;
       })
-  );
+  )
 });
