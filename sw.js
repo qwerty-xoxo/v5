dcc2cac73c1189b57d316d365da0a290f7027675
Infdevv <bkspears@outlook.com>
2024-03-16 22:14:07 -0500

    test

--- sw.js
+++ sw.js
@@ -1,25 +1,51 @@
-
-
+// Axiom proxy Service Worker
 self.addEventListener('fetch', event => {
-  // Get the origin of the service worker script
-  const origin = self.origin;
-
-  // Extract the origin from the request URL
-  const requestOrigin = new URL(event.request.url).origin;
-
-  // If request comes from this origin, don't intercept it.
-  // If it goes to other origins, intercept it.
-  if (requestOrigin === origin) {
-    return;
+  try {
+    // Proxy URL for Axiom
+    const proxy = "https://axiomhub.net/main.php?url=";
+    
+    // Get the origin of the service worker script
+    const origin = self.origin;
+    
+    // If the request comes from this origin, don't intercept it and let it go
+    if (event.request.url.startsWith(origin)) {
+      event.respondWith(
+        fetch(event.request.url)
+          .then(response => {
+            console.log("Response", response);
+            return response;
+          })
+      );
+    } else {
+      // Check if the request is for a web config file
+      if (event.request.url.endsWith('.config')) {
+        // Block the request for web config files
+        event.respondWith(new Response(null, { status: 403, statusText: 'Forbidden' }));
+      } else {
+        // Check if the request is for video or audio streaming
+        const contentType = event.request.headers.get('Content-Type');
+        if (contentType && (contentType.includes('video') || contentType.includes('audio'))) {
+          // Intercept and redirect the request through the proxy for video/audio streaming
+          event.respondWith(
+            fetch(proxy + event.request.url)
+              .then(response => {
+                console.log("Response", response);
+                return response;
+              })
+          );
+        } else {
+          // For other requests, simply fetch directly without redirection
+          event.respondWith(
+            fetch(event.request)
+              .then(response => {
+                console.log("Response", response);
+                return response;
+              })
+          );
+        }
+      }
+    }
+  } catch (error) {
+    console.error("Error", error);
   }
-
-  console.log('Intercepting request for:', event.request.url);
-
-  event.respondWith(
-    fetch("https://axiomhub.net/main.php?url=" + event.request.url)
-      .then(function(response) {
-        console.log("response", response);
-        return response;
-      })
-  )
 });
