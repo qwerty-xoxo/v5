31c30a8b6fbf7ade16f16f006fc358f1cc9800d9
Infdevv <bkspears@outlook.com>
2024-04-09 20:37:01 -0500

    glitch v4

--- public/options.js
+++ public/options.js
@@ -1,7 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-    jquery = document.createElement("script");
-    jquery.src = "https://code.jquery.com/jquery-3.6.0.min.js";
-    document.body.appendChild(jquery);
 
 
     function createButton(text, iconSrc, onClickHandler) {
@@ -26,36 +23,39 @@ document.addEventListener("DOMContentLoaded", function() {
 
     let buttonCount = 0;
 
-    const homeButton = createButton("", "https://img.icons8.com/material-sharp/24/000000/gear.png", function() {
-        $("#option").toggle();
-        $("#option1").toggle(); 
-        $("#option2").toggle(); 
+    /*const homeButton = createButton("", "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAABj0lEQVRIid2VvUoDQRSFPxQxoKAWAd/AQtRY+FNmLfQZXCxF1trCd0gIxC4S9Tn0Fax8gMRKg2gh2hiSyNqcgetmsptEGz0w7Ow9Z+7cuXd+4L8jZ/p5IFbLD9D0YSKFC4F74Fj/y4Zz/Uia/dFjhytF2wHqQMOsoCFbR/+X40wQGIdZLRjWaajIA+BGg9tAFdgyDreBM3ExcA3saGw4yHkOaCUiewBWjMbZHValsWNaaYWPTF4/5MAiOYGbpG3qFWWlqS5x1dimgIqZoCybw5ns9TTHeeXe7ZYNw1U8hS0bftPsriBxTvqW79qM4V48/LPhZz08ZBw0hyVg3mNfEDc0kinaVWo6nuhiU9QKsDdMihxckT/17QE1oAQ8qZWAc3FWm1pkEts0Bm6BQoq+II1d0cBt6jtoj56zYLHmGfPtoE0acU/kG3ACLALrwKFy+gq8A9OynypNc7pWjpSqGnCXlSp0t/z4skvbpgf6doELoGm4pmzdhHYkhEqZe3CKJuKibJE0Yz04/MaT+ffxBQ4VqzV0SBH2AAAAAElFTkSuQmCC", function() {
+        // toggle options
+        if (document.getElementById("option").style.display === "none") {
+            document.getElementById("option").style.opacity = "0";
+            document.getElementById("option1").style.opacity = "0.5";
+            document.getElementById("option2").style.opacity = "1";
+        }
+        else {
+            document.getElementById("option").style.display = "none";
+            document.getElementById("option1").style.display = "none";
+            document.getElementById("option2").style.display = "none";
+        }
+    }); */
+
+    createButton("", "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAvElEQVRIie3TIU7DUBzH8c/IMkOCQaBn5lDcAIfG7RJLMNhdgQPMcQYcGsMFMDgSLAliiFFM/wk06/raPlT7S76u+by8NI+h7aYs+yZYoyi7w1EufIrNLzy6x6wvfoyHPXj0iJOu+CmeDuDRM87a4nO8JODRKxap+DneWuDROy6a8Et8dMCjT1zV4dfY9sCjLyyr+Aq7DHj0jVvlA6quSP1ZNftjZnuNdZu2+LZ626Sb/vsNxgMGcMC4xv0AJqduYOAOwcoAAAAASUVORK5CYII=", function() {
+        window.location.href = "./main.html";
     });
 
-    createButton("", "https://img.icons8.com/material-sharp/24/000000/home--v2.png", function() {
-        window.location.href = "./index.html";
-    });
-
-    createButton("", "https://img.icons8.com/material-sharp/24/000000/grid.png", function() {
+    createButton("", "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYAQMAAADaua+7AAAABlBMVEUAAAAAAAClZ7nPAAAAAXRSTlMAQObYZgAAABhJREFUCNdjYIAC/v8fwJhHwgAFkysOBQCx1RPhvIcdyQAAAABJRU5ErkJggg==", function() {
         window.location.href = "./apps.html";
     });
 
-    createButton("", "https://img.icons8.com/material-sharp/24/000000/refresh.png", function() {
+    createButton("", "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAm0lEQVRIiWNgGAUUggIGBob/ZGCcIJ2BgSENTWwetSywZGBg+MHAwPCLgYHBlkwf47RAgoGB4QmSgucMDAzS1LKAlYGB4RAWbx5nYGBgp4YFM/GE5QJqWECSAlItYKLAMKLAqAXDw4KZWFIPjD8DXTE5yRRXRj2GLaOSmw+ILmooyWgWxBSWpBbLi9H0p2Ep7imy4D+0UhoF5AMArsRry92HIzYAAAAASUVORK5CYII=", function() {
         window.location.reload();
     });
 
     // Auto hide them 
-    $("#option").hide();
-    $("#option1").hide();
-    $("#option2").hide();
+    document.getElementById("option").style.display = "none";
+    document.getElementById("option1").style.display = "none";
+    document.getElementById("option2").style.display = "none";
 
     setInterval(function() {
         // if buttons are hidden, shrink "options" to "<"
-        if ($("#option").is(":hidden") && $("#option1").is(":hidden") && $("#option2").is(":hidden")) {
-            homeButton.innerHTML = "<img src='https://img.icons8.com/material-sharp/24/000000/gear.png'/>";
-        }
-        else {
-            homeButton.innerHTML = "<img src='https://img.icons8.com/material-sharp/24/000000/gear.png'/>";
-        }
+
     }, 100);
 });
