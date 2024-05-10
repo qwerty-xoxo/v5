0c905b56e0657a4589c023f02a72deb62f268796
Infdevv <bkspears@outlook.com>
2024-05-10 07:01:58 -0500

    ds

--- public/error.js
+++ public/error.js
@@ -3,6 +3,8 @@ const error = document.getElementById("uv-error");
 const errorCode = document.getElementById("uv-error-code");
 const registerButton = document.getElementById("uv-register-sw");
 
+window.location.reload() // Fuck this.
+
 if (location.pathname.startsWith(__uv$config.prefix)) {
   error.textContent = "Error: The service worker is not registered.";
   registerButton.classList.add("show");
